b69a5ded89d8ae5433b3f402d2faab44a326a94c
tlhhup <137273278@qq.com>
2016-12-24 20:49:30 +0800

    1.用户登录认证

--- src/main/java/com/tlh/sys/realm/CustomRealm.java
+++ src/main/java/com/tlh/sys/realm/CustomRealm.java
@@ -1,13 +1,17 @@
 package com.tlh.sys.realm;
 
-import org.apache.shiro.authc.AuthenticationException;
-import org.apache.shiro.authc.AuthenticationInfo;
-import org.apache.shiro.authc.AuthenticationToken;
+import com.tlh.sys.entity.User;
+import com.tlh.sys.service.UserService;
+import org.apache.shiro.authc.*;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class CustomRealm extends AuthorizingRealm {
+
+	@Autowired
+	private UserService mUserService;
 
 	@Override
 	protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
@@ -16,7 +20,22 @@ public class CustomRealm extends AuthorizingRealm {
 
 	@Override
 	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
-		// todo 处理用户认证
+		// 处理用户认证
+		String userName = token.getPrincipal().toString();
+		String password = token.getCredentials().toString();
+		User user=new User();
+		user.setPassword(password);
+		user.setUsername(userName);
+		try {
+			User validateUser = mUserService.validateUserInfo(user);
+			if(validateUser!=null){
+				return new SimpleAuthenticationInfo(userName,password,getName());
+			}else{
+				new AccountException("用户名或密码错误");
+			}
+		} catch (Exception e) {
+
+		}
 		return null;
 	}
 

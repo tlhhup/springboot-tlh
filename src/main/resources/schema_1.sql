806ffffd164e79ef4827cde29279c60fefd0e6d5
tlhhup <137273278@qq.com>
2017-01-11 20:08:17 +0800

    1.修改数据库表结构

--- src/main/resources/schema_1.sql
+++ src/main/resources/schema_1.sql
@@ -13,6 +13,7 @@ create table sys_roles(
 id int primary key auto_increment comment '主键',
 roleName varchar(100) comment '角色名'
 );
+ALTER table sys_roles add (description VARCHAR(100) COMMENT '角色描述',available tinyint COMMENT '是否可用');
 -- 用户角色表
 create table sys_user_role_links(
 role_id int,
@@ -27,6 +28,7 @@ id int primary key auto_increment comment '主键',
 name varchar(100) comment '权限名称',
 url varchar(100) comment '资源地址'
 );
+ALTER TABLE sys_rights ADD (priority int COMMENT '显示顺序',parent_id int COMMENT '父级编号',available TINYINT COMMENT '是否可用',type VARCHAR(50) COMMENT '资源类型')
 -- 角色权限表
 create table sys_role_right_links(
 role_id int,
